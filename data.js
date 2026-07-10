cc5059167b22e32c81fdeaf1f1d7584a7058627c
edenleroy59450-tech <eden.leroy59450@gmail.com>
2026-07-11 00:27:34 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,35 @@
+const KIPLOKONES = [
+
+{
+    id:1,
+    nom:"Flamou",
+    type:"Feu",
+    rarete:"Commune",
+    image:"images/kiplokones/flamou.png"
+},
+
+{
+    id:2,
+    nom:"Aquili",
+    type:"Eau",
+    rarete:"Commune",
+    image:"images/kiplokones/aquili.png"
+},
+
+{
+    id:3,
+    nom:"Feuiflor",
+    type:"Plante",
+    rarete:"Rare",
+    image:"images/kiplokones/feuiflor.png"
+},
+
+{
+    id:4,
+    nom:"Voltikro",
+    type:"Électrik",
+    rarete:"Ultra Rare",
+    image:"images/kiplokones/voltikro.png"
+}
+
+];
