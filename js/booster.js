60296957e8a4556a3c3674edd2a9397a942a9300
edenleroy59450-tech <eden.leroy59450@gmail.com>
2026-07-11 13:19:02 +0200

    Update booster.js

--- js/booster.js
+++ js/booster.js
@@ -6,34 +6,25 @@ document.getElementById("openBooster").onclick = function () {
 
     resultat.innerHTML = "";
 
+
     let random = Math.floor(Math.random() * KIPLOKONES.length);
 
     let carte = KIPLOKONES[random];
+
 
     save.collection.push(carte.id);
 
     save.boosters++;
 
+
     sauvegarder(save);
 
-    if (carte.image) {
 
-        resultat.innerHTML = `
-            <div class="card">
-                <img src="${carte.image}" alt="${carte.nom}" class="card-image">
-            </div>
-        `;
 
-    } else {
+    resultat.innerHTML = `
+        <div class="card">
+            <img src="${carte.image}" alt="${carte.nom}">
+        </div>
+    `;
 
-        resultat.innerHTML = `
-            <div class="card">
-                <h2>${carte.nom}</h2>
-                <p>Type : ${carte.type}</p>
-                <p>Rareté : ${carte.rarete}</p>
-            </div>
-        `;
-
-    }
-
-}
+};
